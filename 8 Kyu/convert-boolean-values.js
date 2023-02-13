75fdb6fad8e36fad98abf3ec64de39ac4f4ff9c6
joeacioffi <joeacioffi@gmail.com>
2023-02-13 11:08:22 -0600

    2/13

---
+++ 8 Kyu/convert-boolean-values.js
@@ -0,0 +1,12 @@
+function boolToWord( bool ){
+    if (bool == true) {
+      return "Yes"
+    } else {
+      return "No"
+    }
+  }
+
+
+  function booltoWord2( bool ){
+    return bool ? 'Yes' : 'No';
+  }
