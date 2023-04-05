f9c10d332433c91ad9ba65de883e7386eda90148
joeacioffi <joeacioffi@gmail.com>
2023-04-05 18:59:35 -0500

    4/5

---
+++ 8 Kyu/simple-multiplication.js
@@ -0,0 +1,11 @@
+// assume all values are numbers
+
+function simpleMultiplication(number) {
+    // if the number is even, multiply by 8
+  if(number % 2 === 0){
+    return number * 8
+     // otherwise, multiply by 9
+  } else{
+    return number * 9
+  }
+}
