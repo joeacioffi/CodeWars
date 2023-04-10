5f9033b73335d7122142d065bf183d3adc50cc43
joeacioffi <joeacioffi@gmail.com>
2023-04-10 13:52:19 -0500

    4/10

---
+++ 8 Kyu/welcome.js
@@ -0,0 +1,21 @@
+greet = language =>
+(hello => hello[language] || 'Welcome')
+({
+english: 'Welcome',
+czech: 'Vitejte',
+danish: 'Velkomst',
+dutch: 'Welkom',
+estonian: 'Tere tulemast',
+finnish: 'Tervetuloa',
+flemish: 'Welgekomen',
+french: 'Bienvenue',
+german: 'Willkommen',
+irish: 'Failte',
+italian: 'Benvenuto',
+latvian: 'Gaidits',
+lithuanian: 'Laukiamas',
+polish: 'Witamy',
+spanish: 'Bienvenido',
+swedish: 'Valkommen',
+welsh: 'Croeso'
+})
