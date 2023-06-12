10cf05d7f92125b55ccaeff2bed3fa84547e7a13
joeacioffi <joeacioffi@gmail.com>
2023-06-12 12:33:02 -0500

    6/12

---
+++ 7 Kyu/string-ends-with.js
@@ -0,0 +1,6 @@
+// Solution returns true if the first argument passed in ends with the 2nd argument
+
+function solution(str, ending){
+    //Use endsWith to find the ending of the str, and compare it to the ending
+    return str.endsWith(ending);
+  }
