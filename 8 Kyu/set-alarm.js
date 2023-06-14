979d55a24075bfbc11fdfff285bcb12615a4855e
joeacioffi <joeacioffi@gmail.com>
2023-06-14 11:38:04 -0500

    5/14

---
+++ 8 Kyu/set-alarm.js
@@ -0,0 +1,5 @@
+// function should return true if you are employed and not on vacation
+
+let setAlarm = (employed, vacation) => {
+    return (employed == true && vacation == false) ? true : false
+    }
