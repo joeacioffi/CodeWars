0e773a4492d7efac0a5ec430069e7f32a95f6f06
joeacioffi <joeacioffi@gmail.com>
2023-07-11 15:23:45 -0500

    7/11

---
+++ 8 Kyu/Messi-goals.js
@@ -0,0 +1,9 @@
+//Use variables to find a defined sum of 3 different scores
+
+//Define the scores
+var laLigaGoals = 43
+var championsLeagueGoals = 10
+var copaDelReyGoals = 5
+
+//Add the scores together
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
