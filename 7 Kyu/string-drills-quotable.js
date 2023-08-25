f72f3f609f91480ac7156bf9a4c0557860dca410
joeacioffi <joeacioffi@gmail.com>
2023-08-25 12:27:38 -0500

    8/25

---
+++ 7 Kyu/string-drills-quotable.js
@@ -0,0 +1,7 @@
+//This function should take two string parameters: a person's name (name) and a quote of theirs (quote), and return a string attributing the quote to the person in the following format:
+
+//'[name] said: "[quote]"'
+
+function quotable(name, quote){
+    return `${name} said: "${quote}"`
+  }
